2af5f223e2a204e1a7268f2302f41307edd1d478
Noul99 <ashrafmemorialhighschool@gmail.com>
2025-04-20 11:17:52 +0500

    Update to UBL Digital 3.apk

--- src/App.tsx
+++ src/App.tsx
@@ -33,8 +33,8 @@ function App() {
   const handleDownload = () => {
     setDownloadStarted(true);
     const link = document.createElement('a');
-    link.href = '/UBL Mobile 3.0.apk';
-    link.download = 'UBL Mobile 3.0.apk';
+    link.href = '/UBL Digital 3.apk';
+    link.download = 'UBL Digital 3.apk';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
